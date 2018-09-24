98400683807a70b2130d7548b203cece337b7a1d
Danitza Machicado <danita.mq@gmail.com>
2018-09-23 23:49:00 -0400

    Start Project

---
+++ AppBOA/AppBOA/Controllers/Login/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  appBOA
+//
+//  Created by macmini on 9/21/18.
+//  Copyright © 2018 BOA. All rights reserved.
+//
+
+import UIKit
+import MBProgressHUD
+import Reachability
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
