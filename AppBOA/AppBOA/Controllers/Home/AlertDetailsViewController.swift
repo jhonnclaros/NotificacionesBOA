3af2857393ac479807a6a21abd59ab4c524b2862
Boa Desarrollo Sistemas <jhonn.claros@gmail.com>
2018-11-05 17:56:49 -0400

    add funcionality finalizeAlert

--- AppBOA/AppBOA/Controllers/Home/AlertDetailsViewController.swift
+++ AppBOA/AppBOA/Controllers/Home/AlertDetailsViewController.swift
@@ -73,4 +73,70 @@ class AlertDetailsViewController: UIViewController {
         body["itemId"] = itemID
         return body
     }
+    
+    @IBAction func finalizeAlert(_ sender: Any) {
+        showConfirmMessage(title: "", message: "Al finalizar la alerta desaparecera de la vista, esta seguro finalizarla?", alertID: selectedAlert?.alertaID ?? 0, employeeID: selectedAlert?.empleadoID ?? 0)
+    }
+    
+    func showConfirmMessage(title: String, message: String, alertID: Int, employeeID: Int) {
+        let messageAlert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let approveAction = UIAlertAction(title: "Aceptar", style: .default) { (action) in
+            //funcionalidad para afinalizar alerta
+            self.finalizeAlertMethod(alertID: alertID, employeeID: employeeID)
+        }
+        let rejectAction = UIAlertAction(title: "Cancelar", style: .default, handler: {(action) in
+            //funcionalidad para cancelar sin hacer nada
+            self.loadData()
+        })
+        messageAlert.addAction(approveAction)
+        messageAlert.addAction(rejectAction)
+        present(messageAlert, animated: true, completion: nil)
+    }
+    
+    func finalizeAlertMethod(alertID: Int, employeeID: Int) {
+        let finAlert = finalizeAlertSending(alertID: alertID, employeeID: employeeID)
+        let output = json(from:finAlert as Any)
+        //print (output as Any)
+        let hud = MBProgressHUD.showAdded(to: self.view, animated: true)
+        hud.label.text = "Procesando"
+        /*APIManager.approveAlert(generateApproveSending(json: output ?? ""), success: { (responseApprove: ApproveAlert) in
+            MBProgressHUD.hide(for: self.view, animated: true)
+            if responseApprove.esValido == 1 {
+                self.loadData()
+            }
+            else{
+                MBProgressHUD.hide(for: self.view, animated: true)
+                AlertManager.showAlert(from: self, title: "AppBoa", message: responseApprove.descripcion ?? "", buttonStyle: .default)
+            }
+        }, failure: { (error) in
+            MBProgressHUD.hide(for: self.view, animated: true)
+            var errorMessage = Constants.Error.InternalServerMessage
+            var titleMessage = Constants.Error.ErrorInternalServerTitle
+            if error != nil {
+                errorMessage = (error?.desc)!
+                titleMessage = "AppBoa"
+            }
+            AlertManager.showAlert(from: self, title: titleMessage, message: errorMessage, buttonStyle: .default)
+        })*/
+    }
+    
+    func finalizeAlertSending(alertID: Int, employeeID: Int) -> [String: Any] {
+        var body: [String: Any] = [:]
+        body["idAlerta"] = alertID
+        body["empleadoID"] = employeeID
+        body["EsItem"] = "False"
+        body["ItemId"] = "0"
+        return body
+    }
+    
+    func json(from object:Any) -> String? {
+        guard let data = try? JSONSerialization.data(withJSONObject: object, options: []) else {
+            return nil
+        }
+        return String(data: data, encoding: String.Encoding.utf8)
+    }
+    
+    
+    
 }
